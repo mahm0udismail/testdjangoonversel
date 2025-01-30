3652139ed93b2b8a5249c3e32bc1d288842dcafc
mahm0udismail <mahmoudismail9114@gmail.com>
2025-01-30 20:34:08 +0200

    test

--- pro/myapp/views.py
+++ pro/myapp/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from datetime import datetime
 
 def hello(request):
     return HttpResponse("Hello, World!")
+
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
