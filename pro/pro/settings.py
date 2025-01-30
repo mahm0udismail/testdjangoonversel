dd39e48ab228ac94d5ccaf1dfbe47ae972c08429
mahm0udismail <mahmoudismail9114@gmail.com>
2025-01-30 20:12:52 +0200

    test2

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*z&^z9d-(i4cki#v^lqxucwr-q)h%el+(ql^&3+4t0l^*l-(_0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
