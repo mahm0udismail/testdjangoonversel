3c29c812e96f5c2a1d8049e3b00be047a58a73cb
mahm0udismail <mahmoudismail9114@gmail.com>
2025-03-30 16:39:47 +0200

    edit bash

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py makemigrations
+python manage.py migrate
+
 python manage.py collectstatic --no-input
 
 python manage.py migrate
