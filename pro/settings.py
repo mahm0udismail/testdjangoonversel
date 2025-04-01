d4c32bcba670f7e4252b9a9618f58f5d6a1c462e
mahm0udismail <mahmoudismail9114@gmail.com>
2025-04-01 15:16:38 +0200

    edit database

--- pro/settings.py
+++ pro/settings.py
@@ -93,13 +93,11 @@ WSGI_APPLICATION = 'pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
