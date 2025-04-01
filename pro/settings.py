868a305a0f42d41f1a44f6a4174bab9e6462a915
mahm0udismail <mahmoudismail9114@gmail.com>
2025-04-01 15:21:27 +0200

    Fix database configuration

--- pro/settings.py
+++ pro/settings.py
@@ -96,7 +96,10 @@ WSGI_APPLICATION = 'pro.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        engine='django.db.backends.postgresql'  # Ensure ENGINE is set
+    )
 }
 
 # Password validation
